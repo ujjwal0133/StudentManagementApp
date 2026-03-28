a3a268ad715e12fdd4963f994d76f00754a639e6
ujjwal <ujjwalv1331@gmail.com>
2026-03-28 18:10:35 +0530

    Added Student and Course Entity

---
+++ src/main/java/com/example/demo/entity/Course.java
@@ -0,0 +1,63 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	
+	@Column(unique = true)
+	private String code;
+	
+	@Column(unique = true)
+	private String name;
+	
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,78 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+		
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)
+	private String rollNo;
+	
+	@Column(unique = true)
+	private String email;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(length = 15)
+	private String phone;
+	
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(String rollNo) {
+		this.rollNo = rollNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+	
+}
